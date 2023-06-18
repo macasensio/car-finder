3d4d48b1e8cae139535e7241be05eb41d222062c
macasensio <macasensio@gmail.com>
2023-06-18 22:28:40 +0200

    removing spaces

--- js/db.js
+++ js/db.js
@@ -8,15 +8,15 @@ const autos = [
 		color: 'Blanco',
 		transmision: 'automatico'
 	},
-	{ 
-        marca: 'Audi', 
-        modelo: 'A4', 
-        year: 2023, 
-        precio: 40000, 
-        puertas: 4, 
-        color: 'Negro', 
-        transmision: 'automatico' 
-    },
+	{
+		marca: 'Audi',
+		modelo: 'A4',
+		year: 2023,
+		precio: 40000,
+		puertas: 4,
+		color: 'Negro',
+		transmision: 'automatico'
+	},
 	{
 		marca: 'Ford',
 		modelo: 'Mustang',
@@ -26,15 +26,15 @@ const autos = [
 		color: 'Blanco',
 		transmision: 'automatico'
 	},
-	{ 
-        marca: 'Audi', 
-        modelo: 'A6', 
-        year: 2020, 
-        precio: 35000, 
-        puertas: 4, 
-        color: 'Negro', 
-        transmision: 'automatico' 
-    },
+	{
+		marca: 'Audi',
+		modelo: 'A6',
+		year: 2020,
+		precio: 35000,
+		puertas: 4,
+		color: 'Negro',
+		transmision: 'automatico'
+	},
 	{
 		marca: 'BMW',
 		modelo: 'Serie 5',
@@ -62,15 +62,15 @@ const autos = [
 		color: 'Rojo',
 		transmision: 'manual'
 	},
-	{ 
-        marca: 'Ford', 
-        modelo: 'Mustang', 
-        year: 2019, 
-        precio: 80000, 
-        puertas: 2, 
-        color: 'Rojo', 
-        transmision: 'manual' 
-    },
+	{
+		marca: 'Ford',
+		modelo: 'Mustang',
+		year: 2019,
+		precio: 80000,
+		puertas: 2,
+		color: 'Rojo',
+		transmision: 'manual'
+	},
 	{
 		marca: 'Dodge',
 		modelo: 'Challenger',
@@ -80,16 +80,16 @@ const autos = [
 		color: 'Blanco',
 		transmision: 'automatico'
 	},
-	{ 
-        marca: 'Audi', 
-        modelo: 'A3', 
-        year: 2017, 
-        precio: 55000, 
-        puertas: 2, 
-        color: 'Negro', 
-        transmision: 
-        'manual' 
-    },
+	{
+		marca: 'Audi',
+		modelo: 'A3',
+		year: 2017,
+		precio: 55000,
+		puertas: 2,
+		color: 'Negro',
+		transmision:
+			'manual'
+	},
 	{
 		marca: 'Dodge',
 		modelo: 'Challenger',
@@ -117,15 +117,15 @@ const autos = [
 		color: 'Blanco',
 		transmision: 'automatico'
 	},
-	{ 
-        marca: 'Ford', 
-        modelo: 'Mustang', 
-        year: 2017, 
-        precio: 60000, 
-        puertas: 2, 
-        color: 'Negro', 
-        transmision: 'manual' 
-    },
+	{
+		marca: 'Ford',
+		modelo: 'Mustang',
+		year: 2017,
+		precio: 60000,
+		puertas: 2,
+		color: 'Negro',
+		transmision: 'manual'
+	},
 	{
 		marca: 'Dodge',
 		modelo: 'Challenger',
@@ -162,13 +162,13 @@ const autos = [
 		color: 'Blanco',
 		transmision: 'automatico'
 	},
-	{ 
-        marca: 'Audi', 
-        modelo: 'A4', 
-        year: 2016, 
-        precio: 30000, 
-        puertas: 4, 
-        color: 'Azul', 
-        transmision: 'automatico' 
-    }
+	{
+		marca: 'Audi',
+		modelo: 'A4',
+		year: 2016,
+		precio: 30000,
+		puertas: 4,
+		color: 'Azul',
+		transmision: 'automatico'
+	}
 ];
